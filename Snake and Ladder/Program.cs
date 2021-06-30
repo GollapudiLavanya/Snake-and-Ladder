c7f809a2c75946841e0807a90142d7cafcc00ad0
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-06-30 15:30:11 +0530

    [LAVANYA]SNAKE AND LADDER1

---
+++ Snake and Ladder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class Program
+    {
+        public const int START = 0;
+        public const int FINISH = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake and Ladder game.");
+
+            int player_position = 0;
+            Console.WriteLine("player current position is " + player_position);
+        }
+    }
+}
